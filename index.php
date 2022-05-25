a9b6c918a0c85a9967312526e7fc07cb3a88cb13
HimalTamang <80043996+HimalTamang@users.noreply.github.com>
2022-05-25 22:16:47 +0545

    files
    internship assignment some portion of design into code

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- boostrap 5 cdn link -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- cdn link for boostrap icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <title>Info Tech 101 website portfolio</title>
+    </head>
+
+    <body>
+        <div class="main-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><img src="./images/logo.png" alt="INFOTECH101"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav nav-item-lists ">
+                            <li class="nav-item home">
+                                <a class="nav-link " aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link   " href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <button class="btn-login">Login</button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="second-wrapper">
+            <div class="vector-7"></div>
+            <div class="ellipse719"></div>
+            <div class="ellipse725"></div>
+            <div class="ellipse720"></div>
+            <div class="ellipse726"></div>
+            <div class="ellipse723"></div>
+            <div class="ellipse23"></div>
+            <div class="ellipse24"></div>
+            <div class="ellipse2526"></div>
+            <div class="ellipse29"></div>
+            <div class="landingpage-image">
+                <img src="./images/landingpage.png" alt="Landingpage">
+            </div>
+            <div class="text-in-landingpage">
+                <h1>We Hear <br> We Make It Happen</h1>
+                <h3>Website Development and Graphic Design</h3>
+                <h4>We provide a complete web and app solution by providing you the esential services including
+                    branding, graphic design, UX/UI design, web development, content, social media integration, SEO, and
+                    online marketing.</h4>
+
+                <button><span>Connect Now</span><i class="bi bi-share"></i></button>
+            </div>
+        </div>
+        <div class="third-wrapper d-block align-items-center">
+            <div class="ellipse10"></div>
+            <div class="ellipse12"></div>
+            <center>
+                <div class="content-text">
+                    <h2> Movement For Brand Digitization</h2>
+                    <h4>101 INFOTECH is an extension of your organization. You aren’t just another client. You are a
+                        partner, and you deserve an experience that is comfortable and tailored to your needs. We are
+                        the team you deserve. We don’t claim to be the best in Nepal or Kathmandu, but we do our best
+                        and deliver what you need.</h4>
+                </div>
+            </center>
+            <div class="ellipse11"></div>
+            <div class="container-fluid con2">
+                <div class="row">
+                    <div class="col-6 list1">
+                        <i class="bi bi-calendar-fill"></i>
+                        <h5> A Timeline You Set</h5>
+                        <p>Stop feeling stuck. We only use month-to-month contracts</p>
+                    </div>
+                    <div class="col-6 list2 ">
+                        <i class="bi bi-tags"></i>
+                        <h5>A Price That Fits You</h5>
+                        <p>Hit goals with a customized strategy you can afford.</p>
+                    </div>
+                    <div class="col-6 list3">
+                        <i class="bi bi-people-fill"></i>
+                        <h5>A Team You Know and Trust</h5>
+                        <p>Collaborate with our in-house team. We don’t outsource.</p>
+                    </div>
+                    <div class="col-6 list4">
+                        <i class="bi bi-graph-up-arrow " "></i>
+                        <h5> A Team A Holistic Transformation of Your Businessline You Set</h5>
+                        <p>Enhance marketing, sales, and customer service efforts.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class=" container con4">
+                            <div>
+                                <ul class="row">
+                                    <li class="col-6 service1"> Get <a href="#">Exceptional</a>
+                                        Service for Growth</li>
+                                    <li class="col-6 service2">
+                                        Our experienced team is here to help you grow your business to a new height. We
+                                        guarantee to provide services at it’s best to help you convert your idea into a
+                                        brand.</li>
+                                </ul>
+                            </div>
+                            <div class="card-body-container">
+                                <ul class="row">
+                                    <li class="col-4 web">
+                                        <img src="./images/web.png" alt="">
+                                        <h3>Web Development</h3>
+                                    </li>
+                                    <li class="col-4 web">
+
+                                        <h3>Application Development</h3>
+                                    </li>
+                                    <li class="col-4 web">
+                                        <h3>Graphic Design</h3>
+                                    </li>
+                                    <li class="col-4 web">
+                                        <h3>Digital Marketing</h3>
+                                    </li>
+                                    <li class="col-4 web">
+                                        <h3> Blog</h3>
+                                    </li>
+                                    <li class="col-4 web">
+                                        <h3>Conact Support</h3>
+                                    </li>
+                                </ul>
+                            </div>
+                    </div>
+                    <!-- Option 2: Separate Popper and Bootstrap JS -->
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+                        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+                        crossorigin="anonymous">
+                    </script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+                        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+                        crossorigin="anonymous">
+                    </script>
+
+
+    </body>
+
+</html>
